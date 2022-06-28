538a7b29e9af86a62f1faf6c9eb77be3a452d76d
21A91A05J1 <97425115+21A91A05J1@users.noreply.github.com>
2022-06-28 19:38:40 +0530

    #include<stdio.h> #include<math.h> int prime(int n) {     int i;     if(n<2)     {         return 0;     }     else     {         for(i=2;i<=sqrt(n);i++)         {             if(n%i==0)             {                 return 0;             }         }         return 1;     } } int main() {     int m,n,i,c=0;     scanf("%d%d",&m,&n);     for (i=m;i<=n;i++)     {         if(prime(i))         {             c++;         }     }     printf("%d",c);
    }

---
+++ Primes_in_range.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+int prime(int n)
+{
+    int i;
+    if(n<2)
+    {
+        return 0;
+    }
+    else
+    {
+        for(i=2;i<=sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+int main()
+{
+    int m,n,i,c=0;
+    scanf("%d%d",&m,&n);
+    for (i=m;i<=n;i++)
+    {
+        if(prime(i))
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+}
