ccee33dbb15159983e66ab2fd1965b6eadc05469
21A91A05J1 <97425115+21A91A05J1@users.noreply.github.com>
2022-06-29 06:11:48 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,s=0,x=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==1)         {             x=i;         }     }     printf("%d",x); }

---
+++ last_odd_number_index_position.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,s=0,x=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==1)
+        {
+            x=i;
+        }
+    }
+    printf("%d",x);
+}
