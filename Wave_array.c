a8c2350f45b9352b283f0ab7c8a9d2c3b87e7d12
21A91A05J1 <97425115+21A91A05J1@users.noreply.github.com>
2022-06-30 11:14:23 +0530

    #include<stdio.h> int main() {     int n,i,m,j=0,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     scanf("%d",&a[i]);     for(i=0;i<n-2;i++)     {         if(a[i]<a[i+1] && a[i+1]>a[i+2] || a[i]>a[i+1] && a[i+1]<a[i+2]) { i++; c++; }         else { printf("no");  c=0; break;  }     }     if(c!=0)     printf("yes"); }

---
+++ Wave_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,m,j=0,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n-2;i++)
+    {   
+        if(a[i]<a[i+1] && a[i+1]>a[i+2] || a[i]>a[i+1] && a[i+1]<a[i+2]) { i++; c++; }
+        else { printf("no");  c=0; break;  }
+    }
+    if(c!=0)
+    printf("yes");
+}
