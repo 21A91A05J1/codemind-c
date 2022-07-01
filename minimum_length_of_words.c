d9d65a15e60ba0bd5c748e527743361819646c4a
21A91A05J1 <97425115+21A91A05J1@users.noreply.github.com>
2022-07-01 06:33:08 +0530

    #include<stdio.h> #include<string.h> int main() {     char ch[100];     int c=0,i,x,min=9999;     scanf("%[^ ]",ch);     for(i=0;i<strlen(ch);i++)     {         x=ch[i];         if(x!=32) c++;         if(x==32 || i==(strlen(ch)-1))         {              if(c<min)  min=c;              c=0;         }     }     printf("%d",min); }

---
+++ minimum_length_of_words.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char ch[100];
+    int c=0,i,x,min=9999;
+    scanf("%[^
+]",ch);
+    for(i=0;i<strlen(ch);i++)
+    {
+        x=ch[i];
+        if(x!=32) c++;
+        if(x==32 || i==(strlen(ch)-1))
+        {
+             if(c<min)  min=c;
+             c=0;
+        }
+    }
+    printf("%d",min);
+}
