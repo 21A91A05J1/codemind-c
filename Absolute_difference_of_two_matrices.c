ff3c56431b3694071c20b01372e8716342f10b77
21A91A05J1 <97425115+21A91A05J1@users.noreply.github.com>
2022-07-20 15:11:21 +0530

    #include<stdio.h> #include<math.h> int main() {     int r,i,j,a[100][100],b[100][100],c[100][100];     scanf("%d",&r);     for(i=0;i<r;i++)     {         for(j=0;j<r;j++)         scanf("%d",&a[i][j]);     }     for(i=0;i<r;i++)     {         for(j=0;j<r;j++)         scanf("%d",&b[i][j]);     }      for(i=0;i<r;i++)     {         for(j=0;j<r;j++){         c[i][j]=abs(a[i][j]-b[i][j]);         if(j!=r-1)         printf("%d ",c[i][j]);         else             printf("%d",c[i][j]);         }         if(i!=r-1)         printf(" ");     } }

---
+++ Absolute_difference_of_two_matrices.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int r,i,j,a[100][100],b[100][100],c[100][100];
+    scanf("%d",&r);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<r;j++)
+        scanf("%d",&a[i][j]);
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<r;j++)
+        scanf("%d",&b[i][j]);
+    }
+     for(i=0;i<r;i++)
+    {
+        for(j=0;j<r;j++){
+        c[i][j]=abs(a[i][j]-b[i][j]);
+        if(j!=r-1)
+        printf("%d ",c[i][j]); 
+        else
+            printf("%d",c[i][j]);
+        }
+        if(i!=r-1)
+        printf("
+");
+    }
+}
